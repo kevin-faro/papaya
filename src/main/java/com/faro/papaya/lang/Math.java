ed41b269cac87268e6004f8be206bcddd031424d
Kevin Faro <kevin.faro@gmail.com>
2014-07-14 21:37:07 -0400

    formatted the code

--- src/main/java/com/faro/papaya/lang/Math.java
+++ src/main/java/com/faro/papaya/lang/Math.java
@@ -4,120 +4,141 @@ package com.faro.papaya.lang;
  * Extra math utilities
  */
 public final class Math {
-	private Math() {}
-	
+	private Math() {
+	}
+
 	/**
 	 * Max of many integers
 	 * 
-	 * @param h head
-	 * @param t tail
+	 * @param h
+	 *            head
+	 * @param t
+	 *            tail
 	 * @return max
 	 */
 	public static int max(int h, int... t) {
 		int m = h;
-		for(int i : t) {
-			m =java.lang.Math.max(m, i);
+		for (int i : t) {
+			m = java.lang.Math.max(m, i);
 		}
 		return m;
 	}
+
 	/**
 	 * Min of many integers
 	 * 
-	 * @param h head
-	 * @param t tail
+	 * @param h
+	 *            head
+	 * @param t
+	 *            tail
 	 * @return min
 	 */
 	public static int min(int h, int... t) {
 		int m = h;
-		for(int i : t) {
-			m =java.lang.Math.min(m, i);
+		for (int i : t) {
+			m = java.lang.Math.min(m, i);
 		}
 		return m;
 	}
-	
+
 	/**
 	 * Max of many longs
 	 * 
-	 * @param h head
-	 * @param t tail
+	 * @param h
+	 *            head
+	 * @param t
+	 *            tail
 	 * @return max
 	 */
 	public static long max(long h, long... t) {
 		long m = h;
-		for(long i : t) {
-			m =java.lang.Math.max(m, i);
+		for (long i : t) {
+			m = java.lang.Math.max(m, i);
 		}
 		return m;
 	}
+
 	/**
 	 * Min of many longs
 	 * 
-	 * @param h head
-	 * @param t tail
+	 * @param h
+	 *            head
+	 * @param t
+	 *            tail
 	 * @return min
 	 */
 	public static long min(long h, long... t) {
 		long m = h;
-		for(long i : t) {
-			m =java.lang.Math.min(m, i);
+		for (long i : t) {
+			m = java.lang.Math.min(m, i);
 		}
 		return m;
 	}
-	
+
 	/**
 	 * Max of many floats
 	 * 
-	 * @param h head
-	 * @param t tail
+	 * @param h
+	 *            head
+	 * @param t
+	 *            tail
 	 * @return max
 	 */
 	public static float max(float h, float... t) {
 		float m = h;
-		for(float i : t) {
-			m =java.lang.Math.max(m, i);
+		for (float i : t) {
+			m = java.lang.Math.max(m, i);
 		}
 		return m;
 	}
+
 	/**
 	 * Min of many floats
 	 * 
-	 * @param h head
-	 * @param t tail
+	 * @param h
+	 *            head
+	 * @param t
+	 *            tail
 	 * @return min
 	 */
 	public static float min(float h, float... t) {
 		float m = h;
-		for(float i : t) {
-			m =java.lang.Math.min(m, i);
+		for (float i : t) {
+			m = java.lang.Math.min(m, i);
 		}
 		return m;
 	}
-	
+
 	/**
 	 * Max of many doubles
 	 * 
-	 * @param h head
-	 * @param t tail
+	 * @param h
+	 *            head
+	 * @param t
+	 *            tail
 	 * @return max
 	 */
 	public static double max(double h, double... t) {
 		double m = h;
-		for(double i : t) {
-			m =java.lang.Math.max(m, i);
+		for (double i : t) {
+			m = java.lang.Math.max(m, i);
 		}
 		return m;
 	}
+
 	/**
 	 * Min of many doubles
 	 * 
-	 * @param h head
-	 * @param t tail
+	 * @param h
+	 *            head
+	 * @param t
+	 *            tail
 	 * @return min
 	 */
 	public static double min(double h, double... t) {
 		double m = h;
-		for(double i : t) {
-			m =java.lang.Math.min(m, i);
+		for (double i : t) {
+			m = java.lang.Math.min(m, i);
 		}
 		return m;
 	}
--- src/test/java/com/faro/papaya/lang/MathTest.java
+++ src/test/java/com/faro/papaya/lang/MathTest.java
@@ -8,69 +8,81 @@ import org.junit.Test;
 public class MathTest {
 	@Test
 	public void testIntMax2() {
-		assertEquals(5, max(3,5));
+		assertEquals(5, max(3, 5));
 	}
+
 	@Test
 	public void testIntMax3() {
-		assertEquals(5, max(3,4,5));
+		assertEquals(5, max(3, 4, 5));
 	}
+
 	@Test
 	public void testIntMin2() {
-		assertEquals(3, min(3,5));
+		assertEquals(3, min(3, 5));
 	}
+
 	@Test
 	public void testIntMin3() {
-		assertEquals(3, min(3,4,5));
+		assertEquals(3, min(3, 4, 5));
 	}
-	
+
 	@Test
 	public void testLongMax2() {
-		assertEquals(5L, max(3L,5L));
+		assertEquals(5L, max(3L, 5L));
 	}
+
 	@Test
 	public void testLongMax3() {
-		assertEquals(5L, max(3L,4L,5L));
+		assertEquals(5L, max(3L, 4L, 5L));
 	}
+
 	@Test
 	public void testLongMin2() {
-		assertEquals(3L, min(3L,5L));
+		assertEquals(3L, min(3L, 5L));
 	}
+
 	@Test
 	public void testLongMin3() {
-		assertEquals(3L, min(3L,4L,5L));
+		assertEquals(3L, min(3L, 4L, 5L));
 	}
 
 	@Test
 	public void testFloatMax2() {
-		assertEquals(5.0F, max(3.0F,5.0F), .000001F);
+		assertEquals(5.0F, max(3.0F, 5.0F), .000001F);
 	}
+
 	@Test
 	public void testFloatMax3() {
-		assertEquals(5.0F, max(3.0F,4.0F,5.0F), .000001F);
+		assertEquals(5.0F, max(3.0F, 4.0F, 5.0F), .000001F);
 	}
+
 	@Test
 	public void testFloatMin2() {
-		assertEquals(3.0F, min(3.0F,5.0F), .000001F);
+		assertEquals(3.0F, min(3.0F, 5.0F), .000001F);
 	}
+
 	@Test
 	public void testFloatMin3() {
-		assertEquals(3.0F, min(3.0F,4.0F,5.0F), .000001F);
+		assertEquals(3.0F, min(3.0F, 4.0F, 5.0F), .000001F);
 	}
 
 	@Test
 	public void testDoubleMax2() {
-		assertEquals(5.0D, max(3.0D,5.0D), .000001D);
+		assertEquals(5.0D, max(3.0D, 5.0D), .000001D);
 	}
+
 	@Test
 	public void testDoubleMax3() {
-		assertEquals(5.0D, max(3.0D,4.0D,5.0D), .000001D);
+		assertEquals(5.0D, max(3.0D, 4.0D, 5.0D), .000001D);
 	}
+
 	@Test
 	public void testDoubleMin2() {
-		assertEquals(3.0D, min(3.0D,5.0D), .000001D);
+		assertEquals(3.0D, min(3.0D, 5.0D), .000001D);
 	}
+
 	@Test
 	public void testDoubleMin3() {
-		assertEquals(3.0D, min(3.0D,4.0D,5.0D), .000001D);
+		assertEquals(3.0D, min(3.0D, 4.0D, 5.0D), .000001D);
 	}
 }
